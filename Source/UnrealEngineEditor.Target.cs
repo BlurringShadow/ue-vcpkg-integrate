add10be5d0709cb6655a0c2f2ceebcad9a043f2c
Blurring Shadow <976999484@qq.com>
2020-10-06 21:59:50 +0800

    Initialization commit

---
+++ Source/UnrealEngineEditor.Target.cs
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class UnrealEngineEditorTarget : TargetBase
+{
+	public UnrealEngineEditorTarget(TargetInfo target) : base(target, TargetType.Editor)
+	{
+	}
+}
