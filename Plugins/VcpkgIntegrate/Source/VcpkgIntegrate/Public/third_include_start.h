e7c1a9fd7a2aa17c29bfd2a0f862d1c200cde3e4
Blurring Shadow <976999484@qq.com>
2020-10-07 21:06:13 +0800

    [Refactor] Re-create project

--- Plugins/VcpkgIntegrate/Source/VcpkgIntegrate/Public/third_include_start.h
+++
@@ -1,6 +0,0 @@
-﻿// Created by BlurringShadow at 2020-10-06-下午 12:34
-
-#pragma once
-
-THIRD_PARTY_INCLUDES_START
-#include "undef_macros.generated.h"
--- Source/UnrealEngine/UnrealEngine.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include <UnrealEngine.h>
-#include <Modules/ModuleManager.h>
-// ReSharper disable once CppMissingIndent
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, UnrealEngine, "UnrealEngine")
