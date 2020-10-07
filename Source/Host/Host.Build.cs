d6b23361d0b16209f5bcfa15651ce74392909e6d
Blurring Shadow <976999484@qq.com>
2020-10-07 22:44:07 +0800

    [Add] plugin module

--- Source/Host/Host.Build.cs
+++ Source/Host/Host.Build.cs
@@ -4,9 +4,9 @@ using UnrealBuildTool;
 
 public class Host : ModuleRules
 {
-	public Host(ReadOnlyTargetRules Target) : base(Target)
-	{
-        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore" });
+    public Host(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore", "VcpkgIntegrate" });
 
         PCHUsage = PCHUsageMode.NoSharedPCHs;
         PrivatePCHHeaderFile = "Host.h";
